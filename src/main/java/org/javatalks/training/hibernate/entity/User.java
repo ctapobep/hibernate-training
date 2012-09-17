d90419a02b3922026f917e636bd94c379967531e
ctapobep <ctapobep@javatalks.ru>
2012-09-17 08:07:07 +0200

    Created entities

---
+++ src/main/java/org/javatalks/training/hibernate/entity/User.java
@@ -0,0 +1,23 @@
+package org.javatalks.training.hibernate.entity;
+
+/** @author stanislav bashkirtsev */
+public class User {
+    private long id;
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
