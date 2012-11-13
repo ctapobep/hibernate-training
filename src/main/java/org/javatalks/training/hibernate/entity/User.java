4b1e39e370ffbb7a7b93e994b22c0d7d0a805287
ctapobep <ctapobep@javatalks.ru>
2012-11-13 15:36:27 +0400

    added changes regarding book

--- src/main/java/org/javatalks/training/hibernate/entity/User.java
+++ src/main/java/org/javatalks/training/hibernate/entity/User.java
@@ -1,9 +1,14 @@
 package org.javatalks.training.hibernate.entity;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
 /** @author stanislav bashkirtsev */
 public class User {
     private Long id;
     private String username;
+    private Set<Book> books;
 
     public Long getId() {
         return id;
@@ -19,5 +24,37 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Collection<Book> books) {
+        removeAllBooks();
+        this.books = new HashSet<>(books);
+        for (Book book : this.books) {
+            addBook(book);
+        }
+    }
+
+    public void addBook(Book book) {
+        if (books.add(book)) {
+            book.setAuthor(this);
+        }
+    }
+
+    public void removeAllBooks() {
+        for (Book book : books) {
+            removeBook(book);
+        }
+    }
+
+    public boolean removeBook(Book book) {
+        if (books.remove(book)) {
+            book.setAuthor(null);
+            return true;
+        }
+        return false;
     }
 }
