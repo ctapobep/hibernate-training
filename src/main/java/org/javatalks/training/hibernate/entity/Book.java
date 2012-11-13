919214e00801a657f0b5383cb401e61a489bcfa7
ctapobep <ctapobep@javatalks.ru>
2012-11-13 15:34:49 +0400

    added book in order to show collections

---
+++ src/main/java/org/javatalks/training/hibernate/entity/Book.java
@@ -0,0 +1,32 @@
+package org.javatalks.training.hibernate.entity;
+
+/** @author stanislav bashkirtsev */
+public class Book {
+    private Long id;
+    private String title;
+    private User author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
