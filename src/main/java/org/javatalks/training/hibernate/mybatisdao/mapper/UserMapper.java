79d5d4ed19451a087853d1d809a1e3a84a967f49
ctapobep <ctapobep@javatalks.ru>
2012-11-14 20:11:38 +0400

    Implemented CRUD for User

---
+++ src/main/java/org/javatalks/training/hibernate/mybatisdao/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package org.javatalks.training.hibernate.mybatisdao.mapper;
+
+import org.javatalks.training.hibernate.Crud;
+import org.javatalks.training.hibernate.entity.User;
+
+/** @author stanislav bashkirtsev */
+public interface UserMapper extends Crud<User> {
+}
