e464b202026c77de40146fb2e67d8d5b7fc54498
ctapobep <ctapobep@javatalks.ru>
2012-11-15 19:51:35 +0400

    Added test

--- src/test/java/org/javatalks/training/hibernate/dao/BookDaoTest.groovy
+++ src/test/java/org/javatalks/training/hibernate/dao/BookDaoTest.groovy
@@ -9,21 +9,30 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.transaction.TransactionConfiguration
 import org.springframework.transaction.annotation.Transactional
 
+import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals
+
 /**
  * @author stanislav bashkirtsev
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/org/javatalks/training/hibernate/appContext.xml")
-@TransactionConfiguration(defaultRollback = false)
+@TransactionConfiguration
 @Transactional
 class BookDaoTest {
     @Test
     void "save() sets the ID"() {
         Book book = new Book(title: "Hibernate.. At last")
-        bookDao.insert(book)//now book is managed by hibernate, it's persisted
+        sut.insert(book)//now book is managed by hibernate, it's persisted
         assert book.id != null
     }
 
+    @Test
+    void "get() returns same object as was stored"() {
+        Book book = new Book(title: "Hibernate.. At last")
+        sut.insert(book) //now book is managed by hibernate, it's persisted
+        assertReflectionEquals(book, sut.get(book.id))
+    }
+
     @Autowired
-    private BookDao bookDao;
+    private BookDao sut;
 }
