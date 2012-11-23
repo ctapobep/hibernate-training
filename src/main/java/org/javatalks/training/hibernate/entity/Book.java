6d34fcf6e82471bb2ce0d0e4d8e2a368151cbc4d
ctapobep <ctapobep@javatalks.ru>
2012-11-23 19:36:42 +0400

    Removed unnecessary classes/cases.

--- src/main/java/org/javatalks/training/hibernate/entity/Book.java
+++ src/main/java/org/javatalks/training/hibernate/entity/Book.java
@@ -11,15 +11,6 @@ public class Book {
     private BookCover cover;
     private List<Chapter> chapters;
     private Map<String, Object> properties = new HashMap<>();
-    /**
-     * Book is not the main side of the association, but User is. If both sides would be able to store the whole
-     * association, this might lead to the chaos, because while saving the Book, we would need for instance to load
-     * Author and her collection of Books from DB (because author has a Set of books, and this type of collection
-     * enforces only a single instance of the same object to be present in collection which we would need to check),
-     * <p/> In order to escape the chaos of 'any entity can save the association', we assume that while storing the
-     * Book, its Author should already be present in DB.
-     */
-    private User author;
 
     public void putProperty(String name, Object value){
         properties.put(name, value);
@@ -47,18 +38,6 @@ public class Book {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public Long getAuthorId() {
-        return author == null ? null : author.getId();
     }
 
     public List<Chapter> getChapters() {
