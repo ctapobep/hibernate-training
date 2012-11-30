52104e63352c74ced21baafa7404171f7b2a4882
ctapobep <ctapobep@javatalks.ru>
2012-11-30 17:40:50 +0400

    Added test for fetching strategies

--- src/test/java/org/javatalks/training/hibernate/dao/FetchingTest.groovy
+++ src/test/java/org/javatalks/training/hibernate/dao/FetchingTest.groovy
@@ -3,20 +3,30 @@ package org.javatalks.training.hibernate.dao
 import org.javatalks.training.hibernate.entity.Book
 import org.javatalks.training.hibernate.entity.Comment
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.transaction.TransactionConfiguration
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * @author stanislav bashkirtsev
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/org/javatalks/training/hibernate/appContext.xml")
+@TransactionConfiguration
+@Transactional
 class FetchingTest {
     @Test
     void "try changing fetching properties for list"() {
         Book book = new Book(title: "try me out", comments: Comment.randomComments(500))
         dao.save(book).flushAndClearSession()
 
-        book = dao.get(book.id)
-        dao.flushAndClearSession()
-        book.comments
+        book = dao.get(book.id) // now see the SQL logs and experiment lazy & fetch attributes
+        println "[HIBERNATE TRAINING] Let's load something!"
+        println "[HIBERNATE TRAINING] COLLECTION SIZE: " + book.comments.size()
+        println "[HIBERNATE TRAINING] FIRST ELEMENT: " + book.comments.subList(0, 0)
     }
 
     @Autowired BookDao dao;
