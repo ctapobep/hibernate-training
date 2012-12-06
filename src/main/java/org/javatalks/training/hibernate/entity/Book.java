3f9fc7582649d989e0a1da052f896e5155ab3bbb
ctapobep <ctapobep@javatalks.ru>
2012-12-07 02:41:36 +0400

    Corrected annotations, removed unnecessary files.

--- src/main/java/org/javatalks/training/hibernate/entity/Book.java
+++ src/main/java/org/javatalks/training/hibernate/entity/Book.java
@@ -1,19 +1,15 @@
 package org.javatalks.training.hibernate.entity;
 
+import javax.persistence.*;
+
 /** @author stanislav bashkirtsev */
+@Entity
 public class Book {
     private Long id;
     private String title;
-    /**
-     * Book is not the main side of the association, but User is. If both sides would be able to store the whole
-     * association, this might lead to the chaos, because while saving the Book, we would need for instance to load
-     * Author and her collection of Books from DB (because author has a Set of books, and this type of collection
-     * enforces only a single instance of the same object to be present in collection which we would need to check),
-     * <p/> In order to escape the chaos of 'any entity can save the association', we assume that while storing the
-     * Book, its Author should already be present in DB.
-     */
-    private User author;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long getId() {
         return id;
     }
@@ -28,18 +24,6 @@ public class Book {
 
     public void setTitle(String title) {
         this.title = title;
-    }
-
-    public User getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(User author) {
-        this.author = author;
-    }
-
-    public Long getAuthorId() {
-        return author == null ? null : author.getId();
     }
 
     @Override
