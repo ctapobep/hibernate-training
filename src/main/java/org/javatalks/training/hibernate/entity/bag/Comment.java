a36a3b165f51d65cd5520b7a9e43c88ebacfe1de
ctapobep <ctapobep@javatalks.ru>
2013-02-16 23:15:30 +0400

    Revert "mtm, removed lists from bags"
    This reverts commit e8b5220488deb95f915aeb9cff15d81f8c314acc.

---
+++ src/main/java/org/javatalks/training/hibernate/entity/bag/Comment.java
@@ -0,0 +1,23 @@
+package org.javatalks.training.hibernate.entity.bag;
+
+/** @author stanislav bashkirtsev */
+public class Comment {
+    private long id;
+    private String body;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
