0e13c215937a4a84d1ca06676ad425adfe90225a
ctapobep <ctapobep@javatalks.ru>
2014-05-13 20:46:29 +0400

    Added assertion into properties test.

--- src/test/java/org/javatalks/training/hibernate/dao/PropertiesTest.groovy
+++ src/test/java/org/javatalks/training/hibernate/dao/PropertiesTest.groovy
@@ -9,6 +9,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.transaction.TransactionConfiguration
 import org.springframework.transaction.annotation.Transactional
 
+import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals
+
 /**
  * @author stanislav bashkirtsev
  */
@@ -19,13 +21,19 @@ import org.springframework.transaction.annotation.Transactional
 class PropertiesTest {
     @Test
     void "demonstrate properties"() {
-        User user = new User(age: 25,
+        User original = new User(age: 25,
                 biography: "Some day some one may want to make a movie of your life. Make sure it won't go straight " +
                         "to video.",
                 avatar: [1, 2, 3, 4, 5, 6],
                 username: "username",
                 birthday: new Date())
-        dao.save(user)
+        dao.save(original)
+        dao.session().flush()
+        dao.session().clear()
+
+        User fromDb = dao.get(original.id)
+        dao.session().clear()
+        assertReflectionEquals(original, fromDb)
     }
 
     @Autowired UserDao dao;
